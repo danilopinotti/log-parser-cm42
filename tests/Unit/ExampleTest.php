a2ca6426a15d53b6d0b1eea11f588cee322d1030
Danilo Pinotti <danilopinotti@hotmail.com>
2021-09-07 19:46:13 -0300

    Setup tests

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace LogParser\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
