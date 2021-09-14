b6f7bac66a5e43792515734fa55bc201ed207b18
Danilo Pinotti <danilopinotti@hotmail.com>
2021-09-14 11:54:04 -0300

    Tratar erro de arquivo de log não encontrado

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
 use LogParser\LogParser;
+use LogParser\Exceptions\LogFileNotFoundException;
 
 require __DIR__.'/vendor/autoload.php';
 
 $logName = $argv[1] ?? 'games.log';
 $logPath = __DIR__.'/data/'.$logName;
 
-$logParser = LogParser::fromFile($logPath);
-
-echo $logParser->parseJson();
+try {
+    echo LogParser::fromFile($logPath)
+        ->parseJson();
+} catch (LogFileNotFoundException $exception) {
+    echo "Arquivo de log não encontrado '/data/$logName'";
+}
